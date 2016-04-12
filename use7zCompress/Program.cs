9c5f1417d8d6b6093f030839d5d92d024bda5ee1
XavierChen2015 <XavierChen2015@gmail.com>
2016-04-12 18:44:17 +0800

    add compress code and dll

--- use7zCompress/Program.cs
+++ use7zCompress/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SevenZip;
 
 namespace use7zCompress
 {
@@ -10,6 +11,19 @@ namespace use7zCompress
     {
         static void Main(string[] args)
         {
+            //壓縮
+            //SevenZipCompressor.SetLibraryPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "7z.dll"));
+            SevenZipCompressor.SetLibraryPath(@"C:\Program Files\7-Zip\7z.dll");
+            SevenZipCompressor sevenZipCompressor = new SevenZipCompressor();
+            string[] files = { @"D:\WebApiForDidi_Txt\didiSqlStr_20160323_1811.txt" };
+            //壓縮種類
+            sevenZipCompressor.ArchiveFormat = OutArchiveFormat.Zip;
+            sevenZipCompressor.VolumeSize = 0;
+            sevenZipCompressor.CompressFiles(@"d:\test.zip", files);
+            sevenZipCompressor.CompressDirectory(@"D:\WebApiForDidi_Txt", @"d:\testdddddddd.zip");
+            //解壓縮
+            SevenZipExtractor sevenZipExtractor = new SevenZipExtractor(@"d:\test.zip");
+            sevenZipExtractor.ExtractArchive(@"c:\test");
         }
     }
 }
